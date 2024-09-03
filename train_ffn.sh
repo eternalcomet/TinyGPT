7c662513e4c254c7cb1f07c724b8bf0049c2c1ce
Chen Yingfa <chenyingfa1999@qq.com>
2024-09-03 20:27:34 +0800

    fix

--- train_ffn.sh
+++ train_ffn.sh
@@ -5,10 +5,11 @@
 #SBATCH --gres=gpu:8
 #SBATCH --cpus-per-task=8
 
-proj_name="kv-merging"
-run_name="vanilla"
-# run_name="tie_kv"
+proj_name="ffn"
+run_name="vanilla_ng"
+# run_name="tie_kv_ng"
 
+ffn_is_gated="0"
 ffn_tie_kv="0"
 n_train_iters="50000"
 n_decay_iters="5000"
@@ -17,6 +18,7 @@ n_warmup_iters="1000"
 export WANDB_MODE=offline
 cmd="torchrun --standalone --nproc_per_node=8 train_ffn.py"
 cmd+=" --ffn_tie_kv ${ffn_tie_kv}"
+cmd+=" --ffn_is_gated ${ffn_is_gated}"
 cmd+=" --n_train_iters ${n_train_iters}"
 cmd+=" --n_decay_iters ${n_decay_iters}"
 cmd+=" --n_warmup_iters ${n_warmup_iters}"

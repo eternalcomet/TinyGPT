74250323614af67d179db7e559b7019fd75baec4
Chen Yingfa <chenyingfa1999@qq.com>
2024-09-04 21:00:52 +0800

    better code in ffn

--- models/ffn.py
+++ models/ffn.py
@@ -62,6 +62,8 @@ class MHF(nn.Module):
         dim_v: int,
         tie_kv: bool = False,
         d_mid: Optional[int] = None,
+        use_proj_q: bool = True,
+        use_proj_o: bool = True,
         act_name: str = "silu",
     ):
         super().__init__()
@@ -74,38 +76,62 @@ class MHF(nn.Module):
         self.act_name = act_name
 
         if d_mid is None:
-            d_mid = d_model * 3
+            if use_proj_q and use_proj_o:
+                d_mid = d_model * 3
+            elif use_proj_q or use_proj_o:
+                d_mid = int(d_model * 3.5)
+            else:
+                d_mid = d_model * 4
 
         total_dim_k = n_heads * dim_k
         total_dim_v = n_heads * dim_v
         self.dim_k = dim_k
         self.dim_v = dim_v
+        self.use_proj_q = use_proj_q
+        self.use_proj_o = use_proj_o
 
-        self.Wq = nn.Linear(d_model, total_dim_k, bias=False)
-        self.Wo = nn.Linear(total_dim_v, d_model, bias=False)
+        if use_proj_q:
+            self.Wq = nn.Linear(d_model, total_dim_k, bias=False)
+        else:
+            assert total_dim_k == d_model
+
+        if use_proj_o:
+            self.Wo = nn.Linear(total_dim_v, d_model, bias=False)
+        else:
+            assert total_dim_v == d_model
+
         if tie_kv:
             assert (
                 dim_k == dim_v
-            ), f"K and V dimensions must match when tying their weights."
-            self.M = nn.Parameter(torch.randn(n_heads, d_mid, dim_k) * 0.02)
+            ), f"K and V dimensions must match when tying their weights, got {dim_k=}, {dim_v=}"
+            self.M = nn.Parameter(torch.zeros(n_heads, d_mid, dim_k))
+            torch.nn.init.normal_(self.M.data, std=0.02)
         else:
-            self.K = nn.Parameter(torch.randn(n_heads, d_mid, dim_k) * 0.02)
-            self.V = nn.Parameter(torch.randn(n_heads, d_mid, dim_v) * 0.02)
+            self.K = nn.Parameter(torch.zeros(n_heads, d_mid, dim_k))
+            self.V = nn.Parameter(torch.zeros(n_heads, d_mid, dim_v))
+            torch.nn.init.normal_(self.K.data, std=0.02)
+            torch.nn.init.normal_(self.V.data, std=0.02)
 
         self.act_fn = get_act_fn(act_name=act_name)
 
     def forward(self, x: Tensor) -> Tensor:
-        B, T, D = x.shape
-        Q = self.Wq(x)  # (B, T, H * DK)
+        batch_size, seqlen, d_model = x.shape
+        if self.use_proj_q:
+            Q = self.Wq(x)  # (B, T, H * DK)
+        else:
+            Q = x
         Q = rearrange(Q, "b t (h dk) -> b t h dk", h=self.n_heads)
         if self.tie_kv:
-            w = einsum(Q, self.M, "b t h dk, h m dk -> b t h m")
-            w = self.act_fn(w)
-            O = einsum(w, self.M, "b t h m, h m dv -> b t h dv")
+            scores = einsum(Q, self.M, "b t h dk, h m dk -> b t h m")
+            scores = self.act_fn(scores)
+            out = einsum(scores, self.M, "b t h m, h m dv -> b t h dv")
         else:
-            w = einsum(Q, self.K, "b t h dk, h m dk -> b t h m")
-            w = self.act_fn(w)
-            O = einsum(w, self.V, "b t h m, h m dv -> b t h dv")
-        O = rearrange(O, "b t h dv -> b t (h dv)")
-        y = self.Wo(O)  # (b, t, d)
+            scores = einsum(Q, self.K, "b t h dk, h m dk -> b t h m")
+            scores = self.act_fn(scores)
+            out = einsum(scores, self.V, "b t h m, h m dv -> b t h dv")
+        out = rearrange(out, "b t h dv -> b t (h dv)")
+        if self.use_proj_o:
+            y = self.Wo(out)  # (b, t, d)
+        else:
+            y = out
         return y
